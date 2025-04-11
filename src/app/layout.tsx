5ec2590e057c41eeb4066af524e4ac153b1a7b32
Mikhail Slizh <mihalych_i@icloud.com>
2025-04-11 16:15:11 +0300

    added og image and removed unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,19 @@
 import type { Metadata } from "next";
-import { Inter, IBM_Plex_Mono } from "next/font/google";
+import { IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 import { AnalyticsProvider } from "./providers";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
 const ibmPlexMono = IBM_Plex_Mono({
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  variable: "--font-ibm-plex-mono",
-  subsets: ["latin", "cyrillic"],
+  weight: ["400", "600"],
+  variable: "--font-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Focus",
-  description: "Focus official website.",
+  description: "Focus on what trully matters.",
   icons: {
     icon: [
       {
@@ -43,9 +38,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${inter.variable} ${ibmPlexMono.variable} scroll-smooth font-mono antialiased`}
+      className={`${ibmPlexMono.variable} scroll-smooth font-mono antialiased`}
     >
-      <body className="divide-y divide-dashed divide-slate-300 bg-slate-100 text-balance text-slate-800 dark:divide-neutral-700 dark:bg-neutral-900 dark:text-neutral-200">
+      <body className="divide-y divide-dashed divide-slate-300 border-slate-300 bg-slate-100 text-balance text-slate-800 dark:divide-neutral-700 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-200">
         <AnalyticsProvider>
           <Header />
           {children}
